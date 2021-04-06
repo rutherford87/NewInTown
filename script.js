970d09c8aa1c2c96e388f6e850f5e743b7518d8d
Tyler Rutherford <rutherford87@gmail.com>
2021-04-06 10:28:19 -0500

    commit including Ticketmaster fetch

--- script.js
+++ script.js
@@ -3,21 +3,35 @@
 //EventListeners
 
 //Function
-fetch("https://mashvisor-api.p.rapidapi.com/airbnb-property/top-reviewed?state=CA&page=1&city=Los%20Angeles&reviews_count=30&zip_code=91342", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-key": "1cb58a581amsh305daaa9cdf8116p1a0875jsn351d7f82b4e7",
-		"x-rapidapi-host": "mashvisor-api.p.rapidapi.com"
-	}
-})
-.then(response => {
-    return response.json();
-	console.log(response);
-})
-.then(function (data){
-    console.log(data)
-    })
-.catch(err => {
-	console.error(err);
-});
+// fetch("https://mashvisor-api.p.rapidapi.com/airbnb-property/top-reviewed?state=CA&page=1&city=Los%20Angeles&reviews_count=30&zip_code=91342", {
+// 	"method": "GET",
+// 	"headers": {
+// 		"x-rapidapi-key": "1cb58a581amsh305daaa9cdf8116p1a0875jsn351d7f82b4e7",
+// 		"x-rapidapi-host": "mashvisor-api.p.rapidapi.com"
+// 	}
+// })
+// .then(response => {
+//     return response.json();
+// 	console.log(response);
+// })
+// .then(function (data){
+//     console.log(data)
+//     })
+// .catch(err => {
+// 	console.error(err);
+// });
 
+$.ajax({
+    type:"GET",
+    url:"https://app.ticketmaster.com/discovery/v2/events.json?size=10&city=Austin&apikey=FCGvVCePHKa7Wz7YvGXHr3IxxVy506VZ",
+    async:true,
+    dataType: "json",
+    success: function(json) {
+                console.log(json);
+                // Parse the response.
+                // Do other things.
+             },
+    error: function(xhr, status, err) {
+                // This time, we do not end up here!
+             }
+  });
