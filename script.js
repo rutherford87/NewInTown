5a4fbc36e674d2cbdb59ce4e0bf50e39b10f4e62
Jenna Garrison <78498827+jenna4285@users.noreply.github.com>
2021-04-06 10:41:24 -0500

    stash for pull

--- script.js
+++ script.js
@@ -1,29 +1,3 @@
-<<<<<<< HEAD
-
-var placesApi = function () {
-    var requestUrl = ("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=30.2672,-97.7431&radius=5&type=restaurant&keyword=restaurant&key=AIzaSyCI0Z7dnH7JAv8YR5DeljkZVehZUKH3gx8", {
-        "mode": "no-cors",
-    });
-  
-    fetch(requestUrl)
-      .then(function (response) {
-        // return response.json();
-      })
-      .then(function (data) {
-        console.log(data);
-        // for (var i = 0; i < data.length; i++) {
-        //   var userName = document.createElement('h3');
-        //   var issueTitle = document.createElement('p');
-        //   userName.textContent = data[i].user.login;
-        //   issueTitle.textContent = data[i].title;
-        //   issueContainer.append(userName);
-        //   issueContainer.append(issueTitle);
-        // }
-      });
-    }
-
-placesApi();
-=======
 //Selectors
 
 //EventListeners
@@ -47,4 +21,3 @@ fetch("https://mashvisor-api.p.rapidapi.com/airbnb-property/top-reviewed?state=C
 	console.error(err);
 });
 
->>>>>>> 3f3a057f1dc4fd00f7803b847cafeb829636a271
