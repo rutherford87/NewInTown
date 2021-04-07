f718c2b9b3b096ac3d19c5e2a83543364d76eb47
Ian Darland <iansmbpro@ians-mbp.lan>
2021-04-07 10:46:20 -0500

    preparing to pull for day3

--- script.js
+++ script.js
@@ -9,56 +9,56 @@ var lng;
 
 // Mashvisor API call for short term rentals
 // create function for fetch for on click 
-var inputCity = 'Austin'
-var inputState = 'TX'
-fetch("https://mashvisor-api.p.rapidapi.com/airbnb-property/top-reviewed?state="+inputState+"&page=1&city="+inputCity+"&reviews_count=30", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-key": "1cb58a581amsh305daaa9cdf8116p1a0875jsn351d7f82b4e7",
-		"x-rapidapi-host": "mashvisor-api.p.rapidapi.com"
-	}
-})
-.then(response => {
-    return response.json();
-	console.log(response);
-})
-.then(function (data){
-    console.log(data)
-    renderPropCards(data)
-    })
-.catch(err => {
-	console.error(err);
+// var inputCity = 'Austin'
+// var inputState = 'TX'
+// fetch("https://mashvisor-api.p.rapidapi.com/airbnb-property/top-reviewed?state="+inputState+"&page=1&city="+inputCity+"&reviews_count=30", {
+// 	"method": "GET",
+// 	"headers": {
+// 		"x-rapidapi-key": "1cb58a581amsh305daaa9cdf8116p1a0875jsn351d7f82b4e7",
+// 		"x-rapidapi-host": "mashvisor-api.p.rapidapi.com"
+// 	}
+// })
+// .then(response => {
+//     return response.json();
+// 	console.log(response);
+// })
+// .then(function (data){
+//     console.log(data)
+//     renderPropCards(data)
+//     })
+// .catch(err => {
+// 	console.error(err);
 
-});
+// });
 
-function renderPropCards (data){
-    //loop through top 3 rentals
+// function renderPropCards (data){
+//     //loop through top 3 rentals
 
 
-    for(i=0; i<3; i++){
-$('#propCardCont').append(`
-<div class="card-content" id='propCard1'>
-                  <div class="media">
-                    <div class="media-left">
-                      <figure class="image is-48x48">
-                        <img src=${data.content.list[i].thumbnail_url} alt="Placeholder image">
-                      </figure>
-                    </div>
-                    <div class="media-content">
-                      <p class="title is-4">${data.content.list[i].name}</p>
-                    <p class="subtitle is-6">Rate per Night (USD) ${data.content.list[i].price}</p>
-                    </div>
-                  </div>
-                  <div class="content">
-${data.content.list[i].description.substring(0,150)}...
-</div>  
-<a href=${data.content.list[i].map_image_url}>See Map</a> 
-<input class="button" id="searchEventNear" type="submit" value="Search Events Nearby!" data-lat=${data.content.list[i].lat} data-lng=${data.content.list[i].lng}>
+//     for(i=0; i<3; i++){
+// $('#propCardCont').append(`
+// <div class="card-content" id='propCard1'>
+//                   <div class="media">
+//                     <div class="media-left">
+//                       <figure class="image is-48x48">
+//                         <img src=${data.content.list[i].thumbnail_url} alt="Placeholder image">
+//                       </figure>
+//                     </div>
+//                     <div class="media-content">
+//                       <p class="title is-4">${data.content.list[i].name}</p>
+//                     <p class="subtitle is-6">Rate per Night (USD) ${data.content.list[i].price}</p>
+//                     </div>
+//                   </div>
+//                   <div class="content">
+// ${data.content.list[i].description.substring(0,150)}...
+// </div>  
+// <a href=${data.content.list[i].map_image_url}>See Map</a> 
+// <input class="button" id="searchEventNear" type="submit" value="Search Events Nearby!" data-lat=${data.content.list[i].lat} data-lng=${data.content.list[i].lng}>
                   
-                </div>
-`)
-    }
-}
+//                 </div>
+// `)
+//     }
+// }
 
 
   var displayEvents = function(eventSearch) {
@@ -89,22 +89,14 @@ ${data.content.list[i].description.substring(0,150)}...
 	}
   }
 
-<<<<<<< HEAD
 //   template literal for date, move modal above this function
 //  before API, need to get local storage & convert to moment format required for URL parameters.
 //Date must be in YYYY-MM-DD T HH:MM:SS
   var getEvents = function () {
-    var apiUrl = 'https://app.ticketmaster.com/discovery/v2/events.json?size=10&classificationName=rock&latlong=30.2672,-97.7431&radius=100&localStartEndDateTime=2021-04-08T14:00:00,2021-08-01T14:00:00&sort=date,name,asc&apikey=FCGvVCePHKa7Wz7YvGXHr3IxxVy506VZ';
-	var apiUrl = `https://app.ticketmaster.com/discovery/v2/events.json?size=10&classificationName=${genreSelect}&latlong=${lat},${lng}&radius=5&localStartEndDateTime=${dateStartSelect},${dateEndSelect}&apikey=FCGvVCePHKa7Wz7YvGXHr3IxxVy506VZ`;
+    var apiUrl = 'https://app.ticketmaster.com/discovery/v2/events.json?size=10&classificationName=rock&latlong=30.2672,-97.7431&radius=100&localStartEndDateTime=2021-04-08T14:00:00,2021-08-01T14:00:00&sort=distance,date,asc&apikey=FCGvVCePHKa7Wz7YvGXHr3IxxVy506VZ';
+	// var apiUrl = `https://app.ticketmaster.com/discovery/v2/events.json?size=10&classificationName=${genreSelect}&latlong=${lat},${lng}&radius=5&localStartEndDateTime=${dateStartSelect},${dateEndSelect}&apikey=FCGvVCePHKa7Wz7YvGXHr3IxxVy506VZ`;
 
 	
-=======
-  //   template literal for date, move modal above this function
-  var getEvents = function () {
-   //   before API, need to get local storage & convert to moment format required for URL parameters.
-    var apiUrl = `https://app.ticketmaster.com/discovery/v2/events.json?size=10&startDateTime=2021-07-20T14:00:00Z&latlong=${lat},${lng}&radius=10&apikey=FCGvVCePHKa7Wz7YvGXHr3IxxVy506VZ`;
-    
->>>>>>> d61c46788bfa3059557e05eab333a0e7c8a767e5
     fetch(apiUrl)
     .then(function (response) {
         if (response.ok) {
