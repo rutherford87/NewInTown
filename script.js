243da8a67c72bfb18a842bb166203c847577e491
Tyler Rutherford <rutherford87@gmail.com>
2021-04-08 11:05:28 -0500

    no pull needed

--- script.js
+++ script.js
@@ -64,21 +64,6 @@ $('#propCardCont').append(`
     }
 }
 
-// $.ajax({
-//     type:"GET",
-//     url:"https://app.ticketmaster.com/discovery/v2/events.json?size=10&city=Austin&apikey=FCGvVCePHKa7Wz7YvGXHr3IxxVy506VZ",
-//     async:true,
-//     dataType: "json",
-//     success: function(json) {
-//                 console.log(json);
-//                 // Parse the response.
-//                 // Do other things.
-//              },
-//     error: function(xhr, status, err) {
-//                 // This time, we do not end up here!
-//              }
-//   });
-
   var displayEvents = function(eventSearch) {
 	$('#event-container').children().remove()
 	for (var i = 0; i < 3; i++){
